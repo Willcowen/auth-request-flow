1d4f4723282a7a384a5de18e0458003b5a47baf9
willyc <willcowendrums@gmail.com>
2022-04-11 15:20:39 +0100

    part one and two complete

--- src/router.js
+++ src/router.js
@@ -1,24 +1,36 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
+const secretKey = "secret";
 
+router.post("/login", (req, res) => {
+  const payload = {
+    username: mockUser.username,
+  };
+  const token = jwt.sign(payload, secretKey);
+  res.json({ token });
 });
 
-router.get('/profile', (req, res) => {
-
+router.get("/profile", (req, res) => {
+  try {
+    const token = req.headers.authorization.substring(7);
+    jwt.verify(token, secretKey);
+    res.json({ profile: mockUser.profile });
+  } catch (err) {
+    res.status(401).json({ error: "profile not found" });
+  }
 });
 
 module.exports = router;
